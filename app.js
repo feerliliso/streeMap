d8dbf8794af136eccc87e2bbbe44024424a90d6e
feerliliso <feerliliso@hotmail.com>
2018-02-23 22:42:05 +0800

    be changed
    from head

--- app.js
+++ app.js
@@ -1,121 +1,76 @@
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat:39.9, lng: 116.3},
+          zoom: 10
+        });
+        var input = /** @type {!HTMLInputElement} */(
+            document.getElementById('pac-input'));
 
-  var map;
+        var types = document.getElementById('type-selector');
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
 
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
 
-  var markers = [];
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
 
-  function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat:39.9, lng: 116.3},
-      zoom: 8,
-      mapTypeControl: false
-    });
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
 
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+          }));
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
 
-    var locations = [
-      {title: '张家口',detail:'<div><a target="_blank" href="https://zh.wikipedia.org/wiki/%E5%BC%A0%E5%AE%B6%E5%8F%A3%E5%B8%82">资料</a></div>'+'<div>中国冬奥会主办城市</div>', location: {lat: 40.792472, lng: 114.888833}},
-      {title: '保定',detail:'<div><a target="_blank" href="https://zh.wikipedia.org/zh-sg/%E4%BF%9D%E5%AE%9A%E5%B8%82">资料</a></div>'+'<div>河北城市</div>', location: {lat: 38.869233, lng: 115.484022}},
-      {title: '廊坊',detail:'<div><a target="_blank" href="https://zh.wikipedia.org/wiki/%E5%BB%8A%E5%9D%8A%E5%B8%82">资料</a></div>'+'<div>河北城市</div>', location: {lat: 39.545523, lng: 116.690439}},
-      {title: '天津',detail:'<div><a target="_blank" href="https://zh.wikipedia.org/wiki/%E5%A4%A9%E6%B4%A5%E5%B8%82">资料</a></div>'+'<div>北方重要港口</div>', location: {lat: 39.348620, lng: 117.364023}},
-      {title: '唐山',detail:'<div><a target="_blank"href="https://zh.wikipedia.org/wiki/%E5%94%90%E5%B1%B1%E5%B8%82">资料</a></div>'+'<div>重工业城市</div>', location: {lat: 39.647777, lng: 118.187355}},
-      {title: '北京',detail:'<div><a target="_blank" href="https://zh.wikipedia.org/zh-sg/%E5%8C%97%E4%BA%AC%E5%B8%82">资料</a></div>'+'<div>中国首都</div>', location: {lat: 39.918785, lng: 116.420414}}
-    ];
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
 
-    var largeInfowindow = new google.maps.InfoWindow({maxWidth: 200});
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+          infowindow.open(map, marker);
+        });
 
-    for (var i = 0; i < locations.length; i++) {
-      var position = locations[i].location;
-      var title = locations[i].title;
-      var detail = locations[i].detail;
-      document.getElementById('list').innerHTML+='<ul>'+title+'<ul>';
-       var marker = new google.maps.Marker({
-        position: position,
-        title: title,
-        detail:detail,
-        animation: google.maps.Animation.DROP,
-        id: i
-      });
-
-      markers.push(marker);
-
-      marker.addListener('click', function() {
-        populateInfoWindow(this, largeInfowindow);
-      });
-    }
-    showListings();
-    document.getElementById('zoom-to-area-text').addEventListener('click', function () {
-                        this.value= " ";
-    });
-    document.getElementById('zoom-to-area').addEventListener('click', function () {
-      //zoomToArea()
-      findAdress();
-    });
-
-  }
-function findAdress() {
-  var address = document.getElementById('zoom-to-area-text').value;
-  document.getElementById('list').innerHTML = '<ul>'+address+'</ul>';
-  hideListings();
-  var marker = new google.maps.Marker({
-   position: position,
-   title: title,
-   detail:detail,
-   animation: google.maps.Animation.DROP,
-   id: i
- });
-  marker.addListener('click', function() {
-    populateInfoWindow(this, largeInfowindow);
-  });
-}
-/*function zoomToArea() {
-  var geocoder = new google.maps.Geocoder();
-  var address = document.getElementById('zoom-to-area-text').value;
-  if(address == ''){
-    window.alert('请输入地址');
-  }else{
-    geocoder.geocode(
-           { address: address,
-             componentRestrictions: {locality: 'bei jing'}
-           }, function(results, status) {
-             if (status == google.maps.GeocoderStatus.OK) {
-               map.setCenter(results[0].geometry.location);
-               map.setZoom(10);
-        }else{
-          window.alert('您要求的地址我没有找到，请换一个方式在试一下。')
+        // Sets a listener on a radio button to change the filter type on Places
+        // Autocomplete.
+        function setupClickListener(id, types) {
+          var radioButton = document.getElementById(id);
+          radioButton.addEventListener('click', function() {
+            autocomplete.setTypes(types);
+          });
         }
+
+        setupClickListener('changetype-all', []);
+        setupClickListener('changetype-address', ['address']);
+        setupClickListener('changetype-establishment', ['establishment']);
+        setupClickListener('changetype-geocode', ['geocode']);
       }
-    );
-  }
-}*/
-
-  function populateInfoWindow(marker, infowindow) {
-
-    if (infowindow.marker != marker) {
-      infowindow.marker = marker;
-      infowindow.setContent('<div>' + marker.title + '</div>'+'<div>' + marker.detail + '</div>');
-      infowindow.open(map, marker);
-
-      infowindow.addListener('closeclick', function() {
-        infowindow.marker = null;
-      });
-    }
-  }
-
-
-  function showListings() {
-    var bounds = new google.maps.LatLngBounds();
-
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-      bounds.extend(markers[i].position);
-    }
-    map.fitBounds(bounds);
-  }
-
-
-  function hideListings() {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
-    }
-  }
