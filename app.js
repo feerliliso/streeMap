93ab1502766c0b54d960dbd0ab4b6d78b70fbc69
feerliliso <feerliliso@hotmail.com>
2018-02-27 14:39:08 +0800

    add new fun
    rt

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 
 
 var map;
-
+var places;
 // Create a new blank array for all the listing markers.
 var markers = [];
 
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244},
+    center: {lat:39.934166, lng: 116.381197},
     zoom: 13,
     mapTypeControl: false
   });
@@ -17,12 +17,12 @@ function initMap() {
   // These are the real estate listings that will be shown to the user.
   // Normally we'd have these in a database instead.
   var locations = [
-    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+    {kind:'博物馆',title: '首都博物馆', location: {lat: 39.906572, lng: 116.342069}},
+    {kind:'博物馆',title: '中国妇女儿童博物馆', location: {lat: 39.908468, lng: 116.419818}},
+    {kind:'博物馆',title: '孔庙', location: {lat:39.947125, lng: 116.414596}},
+    {kind:'旅店',title: '金融街洲际酒店', location: {lat:39.919208, lng:116.357521}},
+    {kind:'旅店',title: '北京丽晶酒店', location: {lat:39.916344, lng: 116.419020}},
+
   ];
 
   var largeInfowindow = new google.maps.InfoWindow();
@@ -33,20 +33,26 @@ function initMap() {
     var position = locations[i].location;
     var title = locations[i].title;
     // Create a marker per location, and put into markers array.
+     var image = 'fly.png';
      var marker = new google.maps.Marker({
+     icon: image,
+     draggable:true,
       position: position,
       title: title,
-      animation: google.maps.Animation.DROP,
+      animation: google.maps.Animation.BOUNCE,
       id: i
     });
     // Push the marker to our array of markers.
-    markers.push(marker);
+    if (locations[i].kind == "博物馆") {
+      markers.push(marker);
+    }
+
     // Create an onclick event to open an infowindow at each marker.
     marker.addListener('click', function() {
       populateInfoWindow(this, largeInfowindow);
     });
   }
-  showListings();
+
 
 }
 
--- kon.js
+++ kon.js
@@ -1,5 +1,32 @@
-var myViewModel = {
-    personName: 'Bob',
-    personAge: 123
-};
-   ko.applyBindings(myViewModel);
+function MyViewModel() {
+    var self = this;
+    var places;
+    self.lastInterest = ko.observable();
+    self.places = ko.observableArray(['旅店', '饭馆', '超市','公园','博物馆']);
+
+    // The current item will be passed as the first parameter, so we know which place was hovered over
+
+    self.logMouseClick = function(places) {
+      if (places == '旅店') {
+        hideListings();
+        showListings();
+        console.log(places);
+        }
+      if (places == '饭馆') {
+
+      }
+      if (places == '超市') {
+
+      }
+      if (places == '公园') {
+
+      }
+      if (places == '博物馆') {
+        hideListings();
+        showListingsM()
+      }
+
+    }
+}
+
+ko.applyBindings(new MyViewModel());
