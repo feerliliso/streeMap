0a58bc542bfe0010d3ea7b6039c48b50bfe48ae3
feerliliso <feerliliso@hotmail.com>
2018-02-28 21:04:39 +0800

    changed more
    changed more

--- app.js
+++ app.js
@@ -1,91 +1,128 @@
-
-
-
 var map;
-var places;
-// Create a new blank array for all the listing markers.
-var markers = [];
+var marker;
+var markers= [];
+var AllLocations = [{
+        kind: '博物馆',
+        title: '首都博物馆',
+        location: {
+            lat: 39.906572,
+            lng: 116.342069
+        }
+    },
+    {
+        kind: '博物馆',
+        title: '中国妇女儿童博物馆',
+        location: {
+            lat: 39.908468,
+            lng: 116.419818
+        }
+    },
+    {
+        kind: '博物馆',
+        title: '孔庙',
+        location: {
+            lat: 39.947125,
+            lng: 116.414596
+        }
+    },
+    {
+        kind: '旅店',
+        title: '金融街洲际酒店',
+        location: {
+            lat: 39.919208,
+            lng: 116.357521
+        }
+    },
+    {
+        kind: '旅店',
+        title: '北京丽晶酒店',
+        location: {
+            lat: 39.916344,
+            lng: 116.419020
+        }
+    },
 
-function initMap() {
-  // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat:39.934166, lng: 116.381197},
-    zoom: 13,
-    mapTypeControl: false
-  });
+];
+function initMap() {//初始化地图
+  map = new google.maps.Map(document.getElementById('map'),{
+    center:{lat: 39.917694, lng:116.398504},//给出中心坐标
+    zoom:13//给出精度
+});
+var locations = [{
+        kind: '博物馆',
+        title: '首都博物馆',
+        location: {
+            lat: 39.906572,
+            lng: 116.342069
+        }
+    },
+    {
+        kind: '博物馆',
+        title: '中国妇女儿童博物馆',
+        location: {
+            lat: 39.908468,
+            lng: 116.419818
+        }
+    },
+    {
+        kind: '博物馆',
+        title: '孔庙',
+        location: {
+            lat: 39.947125,
+            lng: 116.414596
+        }
+    },
+    {
+        kind: '旅店',
+        title: '金融街洲际酒店',
+        location: {
+            lat: 39.919208,
+            lng: 116.357521
+        }
+    },
+    {
+        kind: '旅店',
+        title: '北京丽晶酒店',
+        location: {
+            lat: 39.916344,
+            lng: 116.419020
+        }
+    },];
+    var largeinfowindow = new google.maps.InfoWindow();//设置信息窗口
+    var bounds = new google.maps.LatLngBounds();
+    for(var i = 0;i< locations.length;i++){
+      var position = locations[i].location;
+      var title = locations[i].title;
 
-  // These are the real estate listings that will be shown to the user.
-  // Normally we'd have these in a database instead.
-  var locations = [
-    {kind:'博物馆',title: '首都博物馆', location: {lat: 39.906572, lng: 116.342069}},
-    {kind:'博物馆',title: '中国妇女儿童博物馆', location: {lat: 39.908468, lng: 116.419818}},
-    {kind:'博物馆',title: '孔庙', location: {lat:39.947125, lng: 116.414596}},
-    {kind:'旅店',title: '金融街洲际酒店', location: {lat:39.919208, lng:116.357521}},
-    {kind:'旅店',title: '北京丽晶酒店', location: {lat:39.916344, lng: 116.419020}},
+      var marker = new google.maps.Marker({
 
-  ];
-
-  var largeInfowindow = new google.maps.InfoWindow();
-
-  // The following group uses the location array to create an array of markers on initialize.
-  for (var i = 0; i < locations.length; i++) {
-    // Get the position from the location array.
-    var position = locations[i].location;
-    var title = locations[i].title;
-    // Create a marker per location, and put into markers array.
-     var image = 'fly.png';
-     var marker = new google.maps.Marker({
-     icon: image,
-     draggable:true,
-      position: position,
-      title: title,
-      animation: google.maps.Animation.BOUNCE,
-      id: i
-    });
-    // Push the marker to our array of markers.
-    if (locations[i].kind == "博物馆") {
+        map:map,
+        position:position,
+        title:title,
+        animation:google.maps.Animation.DROP,
+        id:i
+      });
       markers.push(marker);
+      bounds.extend(marker.position);
+      marker.addListener('click',function () {
+        populateInfoWindow(this,largeinfowindow);
+      });
+
     }
-
-    // Create an onclick event to open an infowindow at each marker.
-    marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
-    });
-  }
-
-
+    map.fitBounds(bounds);
 }
 
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
-    infowindow.open(map, marker);
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick', function() {
-      infowindow.marker = null;
-    });
-  }
-}
 
-// This function will loop through the markers array and display them all.
-function showListings() {
-  var bounds = new google.maps.LatLngBounds();
-  // Extend the boundaries of the map for each marker and display the marker
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-    bounds.extend(markers[i].position);
-  }
-  map.fitBounds(bounds);
-}
 
-// This function will loop through the listings and hide them all.
-function hideListings() {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(null);
+  function populateInfoWindow(marker,infowindow) {
+    if(infowindow.marker !=marker){
+      infowindow.marker = marker;
+      infowindow.setContent('<div>'+marker.title+'</div>');
+      infowindow.open(map,marker);
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      infowindow.addListener('closeclick',function () {
+      infowindow.setMarker = null;
+      this.marker.setAnimation(google.maps.Animation.DROP);
+      });
+    }
   }
-}
--- kon.js
+++ kon.js
@@ -1,16 +1,15 @@
 function MyViewModel() {
     var self = this;
-    var places;
-    self.lastInterest = ko.observable();
     self.places = ko.observableArray(['旅店', '饭馆', '超市','公园','博物馆']);
 
     // The current item will be passed as the first parameter, so we know which place was hovered over
 
     self.logMouseClick = function(places) {
       if (places == '旅店') {
+
         hideListings();
         showListings();
-        console.log(places);
+
         }
       if (places == '饭馆') {
 
@@ -22,11 +21,13 @@ function MyViewModel() {
 
       }
       if (places == '博物馆') {
+
         hideListings();
-        showListingsM()
+        showListings();
       }
 
     }
+    
 }
 
 ko.applyBindings(new MyViewModel());
