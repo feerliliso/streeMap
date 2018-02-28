a255768ee1d361b9b66d6d68a0843e915508c1f5
feerliliso <feerliliso@hotmail.com>
2018-02-28 22:37:06 +0800

    changed the struction
    rt

--- app.js
+++ app.js
@@ -47,47 +47,53 @@ function initMap() {//初始化地图
   map = new google.maps.Map(document.getElementById('map'),{
     center:{lat: 39.917694, lng:116.398504},//给出中心坐标
     zoom:13//给出精度
-});
-for(var i=0;i<AllLocations.length;i++){
-  if(AllLocations[i].kind == "旅店"){
-    locations.push(AllLocations[i]);
-  }
-}
-    var largeinfowindow = new google.maps.InfoWindow();//设置信息窗口
-    var bounds = new google.maps.LatLngBounds();
-    for(var i = 0;i< locations.length;i++){
-      var position = locations[i].location;
-      var title = locations[i].title;
-
-      var marker = new google.maps.Marker({
-
-        map:map,
-        position:position,
-        title:title,
-        animation:google.maps.Animation.DROP,
-        id:i
-      });
-      markers.push(marker);
-      bounds.extend(marker.position);
-      marker.addListener('click',function () {
-        populateInfoWindow(this,largeinfowindow);
-      });
-
-    }
-    map.fitBounds(bounds);
+  });
+  renderMarker();
 }
 
-
-
-  function populateInfoWindow(marker,infowindow) {
-    if(infowindow.marker !=marker){
-      infowindow.marker = marker;
-      infowindow.setContent('<div>'+marker.title+'</div>');
-      infowindow.open(map,marker);
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      infowindow.addListener('closeclick',function () {
-      infowindow.setMarker = null;
-      this.marker.setAnimation(google.maps.Animation.DROP);
-      });
+function select() {
+  for(var i=0;i<AllLocations.length;i++){
+    if(AllLocations[i].kind == "旅店"){
+      locations.push(AllLocations[i]);
     }
   }
+}
+function renderMarker(){
+       select();
+      var largeinfowindow = new google.maps.InfoWindow();//设置信息窗口
+      var bounds = new google.maps.LatLngBounds();
+      for(var i = 0;i< locations.length;i++){
+        var position = locations[i].location;
+        var title = locations[i].title;
+
+        var marker = new google.maps.Marker({
+
+          map:map,
+          position:position,
+          title:title,
+          animation:google.maps.Animation.DROP,
+          id:i
+        });
+        markers.push(marker);
+        bounds.extend(marker.position);
+        marker.addListener('click',function () {
+          populateInfoWindow(this,largeinfowindow);
+        });
+
+      }
+      map.fitBounds(bounds);
+
+}
+
+function populateInfoWindow(marker,infowindow) {
+  if(infowindow.marker !=marker){
+    infowindow.marker = marker;
+    infowindow.setContent('<div>'+marker.title+'</div>');
+    infowindow.open(map,marker);
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    infowindow.addListener('closeclick',function () {
+    infowindow.setMarker = null;
+    this.marker.setAnimation(google.maps.Animation.DROP);
+    });
+  }
+}
